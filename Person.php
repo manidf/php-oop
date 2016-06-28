7afee9e020c446f1ccea9ce6cc6884490e5e7570
manidf <mannuel.ferreira@gmail.com>
2016-06-28 11:02:34 +0200

    SETTER setAge

--- Person.php
+++ Person.php
@@ -1,12 +1,25 @@
 <?php
 
 class Person {
+
 	public $name;
 
 	public function __construct($name) {
 		$this->name = $name;
 	}
 
+	// SETTER
+	public function setAge($age)
+	{
+		if ($age < 18)
+		{
+			throw new Exception("You are not old enought to pass through", 1);
+		}
+		$this->age = $age;
+	}
 }
 
-$john = new Person('Manny Ferreira');
+$mannuel = new Person('Manny Ferreira');
+$mannuel->setAge(33);
+
+var_dump($mannuel);
