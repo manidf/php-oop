e3bd9fe582a71770c72409de7cdcded744de3187
manidf <mannuel.ferreira@gmail.com>
2016-10-30 21:55:12 +0200

    tasks arrays

---
+++ tasks/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $task = [
+        'title' => 'Work',
+        'due' => '21-07-2017',
+        'assigned_to' => 'Manny',
+        'completed' => false,
+    ];
+
+    require 'index.view.php';
---
+++ tasks/index.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tasks</title>
+</head>
+<body>
+
+<h1>Task for the day</h1>
+
+<ul>
+    <li>
+        <strong>Name:</strong> <?= $task['title']; ?>
+    </li>
+    <li>
+        <strong>Due:</strong> <?= $task['due']; ?>
+    </li>
+        <li>
+        <strong>Person Responsible:</strong> <?= $task['assigned_to']; ?>
+    </li>
+    <li>
+        <strong>Status:</strong> <?= $task['completed'] ? 'Completed' : 'Incomplete'; ?>
+    </li>
+</ul>
+
+<ul>
+    <!-- Loop though an array foreach name in the names array give me a name -->
+    <?php foreach ($task as $heading => $value) : ?>
+        <li>
+            <strong><?= ucwords($heading); ?>: </strong> <?= $value; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+</body>
+</html>
