c9db20be298a12b51d7fb38bc43c58476952c910
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2016-11-12 13:20:59 +0200

    creat Task CLASS

--- pdo/index.php
+++ pdo/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Task.php';
+
 try {
     // PHP DATA OBJECT
     $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodos', 'root', 'root');
@@ -13,9 +15,15 @@ $statement = $pdo->prepare('select * from todos');
 // execute the query
 $statement->execute();
 
-$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+// dummy generic object
+// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
 
-var_dump($tasks);
+// fetch data and place in a named CLASS
+$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+
+var_dump($tasks[0]->foobar());
+
+// var_dump($tasks);
 // var_dump($tasks[0]->description);
 
 require 'index.view.php';
