c8a00f3415d6fdeb0d96f0b34ce64844fd86e55c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2016-11-12 13:28:38 +0200

    PDO complete

--- pdo/functions.php
+++ pdo/functions.php
@@ -5,3 +5,29 @@ function dd($data) {
     die(var_dump($data));
     echo '</pre>';
 }
+
+function connectDB()
+{
+    try {
+    // PHP DATA OBJECT
+    return new PDO('mysql:host=127.0.0.1;dbname=mytodos', 'root', 'root');
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+// function accepts and intance of $pdo - database instance
+function fetchAllTasks($pdo)
+{
+    // prepare SQL query
+    $statement = $pdo->prepare('select * from todos');
+
+    // execute the query
+    $statement->execute();
+
+    // dummy generic object
+    // $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+
+    // fetch data and place in a named CLASS
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
--- pdo/index.php
+++ pdo/index.php
@@ -1,27 +1,11 @@
 <?php
 
+require 'functions.php';
 require 'Task.php';
 
-try {
-    // PHP DATA OBJECT
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodos', 'root', 'root');
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
+$pdo = connectDB();
 
-// prepare SQL query
-$statement = $pdo->prepare('select * from todos');
-
-// execute the query
-$statement->execute();
-
-// dummy generic object
-// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
-
-// fetch data and place in a named CLASS
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
-var_dump($tasks[0]->foobar());
+$tasks = fetchAllTasks($pdo);
 
 // var_dump($tasks);
 // var_dump($tasks[0]->description);
